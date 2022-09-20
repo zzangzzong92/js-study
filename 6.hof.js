8dbc70810522327695e3a6be091ef9a69253ad2e
zzangzzong92 <zzangzzong92@gmail.com>
2022-09-20 15:32:22 +0900

    finish higher order function and array method

--- 6.hof.js
+++ 6.hof.js
@@ -36,13 +36,59 @@ result = products.findIndex((value) => value.name === "🍪");
 console.log(result);
 
 // 배열의 아이템들이 부분적으로 조건(콜백함수)에 맞는지 확인
-reuslt = products.some((item) => item.name === "🍪");
+result = products.some((item) => item.name === "🍪");
 console.log(result);
 
 // 배열의 아이템들이 전부 조건(콜백함수)에 맞는지 확인
-reuslt = products.every((item) => item.name === "🍪");
+result = products.every((item) => item.name === "🍪");
 console.log(result);
 
 // 조건에 맞는 모든 아이템들을 새로운 배열로
-reuslt = products.filter((item) => item.name === "🍪");
+result = products.filter((item) => item.name === "🍪");
+console.log(result);
+
+console.clear();
+
+// Map 배열의 아이템들을 각각 다른 아이템으로 매핑할 수 있는, 변환해서 새로운 배열 생성
+const nums = [1, 2, 3, 4, 5];
+result = nums.map((item) => item * 2);
+console.log(result);
+result = nums.map((item) => {
+  if (item % 2 === 0) {
+    return item * 2;
+  } else {
+    return item;
+  }
+});
+console.log(result);
+
+// Flatmap: 중, 배열의 메소드 중 flat을 map으로 활용한다고 생각하자
+result = nums.map((item) => [1, 2]);
+console.log(result);
+
+result = nums.flatMap((item) => [1, 2]);
+console.log(result);
+
+result = ["zzang", "zzong"].flatMap((text) => text.split(""));
+console.log(result);
+
+// sort - 배열의 아이템들을 정렬
+// 문자열 형태의 오름차순으로 요소를 정렬하고, 기존의 배열을 변경
+const texts = ["hi", "abc"];
+texts.sort();
+console.log(texts); // 알파벳 a부터 출력
+
+const numbers = [0, 5, 4, 2, 1, 10];
+numbers.sort();
+console.log(numbers); // 0, 1, 10, 2, 4, 5 - 1다음 10이 나오는 이유는 10의 1을 먼저 인식하기 때문이다
+// < 0 a가 앞으로 정렬, 오름차순
+// > 0 b가 앞으로 정렬, 내림차순
+numbers.sort((a, b) => a - b); // 배열의 숫자 하나씩 가져와서 빼면서 비교하게끔 만들어준다.
+console.log(numbers);
+
+// reduce - 배열의 요소들을 접어서 접어서 하나의 값으로
+result = [1, 2, 3, 4, 5].reduce((sum, value) => {
+  sum += value;
+  return sum;
+}, 0);
 console.log(result);
